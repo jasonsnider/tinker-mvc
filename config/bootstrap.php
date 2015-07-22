f8760ec9b5caa141891f3fb63872f24f9cdcf5e6
Jason Snider <root@jasonsnider.com>
2015-07-22 14:06:44 -0500

    Added the PhpFig autoloader instantiation to bootstrap.php. This requires check to be certain ROOT and DS are defined.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -6,4 +6,24 @@
  */
 namespace Tinker;
 
-//This might be a good place to instantiate the autoloader 
+//If we are not calling bootstrap.php from the index, the following constants will not be defined.
+if (!defined('DS')) {
+    //Shorthand for DIRECTORY_SEPARATOR
+    define('DS', DIRECTORY_SEPARATOR);
+}
+
+if (!defined('ROOT')) {
+    //Defines a standard ROOT file path
+    define('ROOT', dirname(dirname(__FILE__)));
+}
+
+//Load and instantiate, loader and register the auto loader.
+require ROOT . DS .
+    'core' . DS .
+    'vendor' . DS .
+    'PhpFig' . DS . 
+    'src' . DS . 
+    'Loader.php';
+
+$Loader = new \PhpFig\Loader;
+$Loader->register();

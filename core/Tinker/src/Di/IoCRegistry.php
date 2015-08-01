d3d9b4d92be7eaddbec7f5d9fbd6527763c1350a
Jason Snider <root@jasonsnider.com>
2015-08-01 09:49:04 -0500

    Implemented an inversion of control regisitry / dependency injection container.

---
+++ core/Tinker/src/Di/IoCRegistry.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * IoCRegistry.php.
+ */
+namespace Tinker\Di;
+
+/**
+ * A registry style singleton for inversion of control.
+ * 
+ * The IoCRegistry allows for the management dependency injection containers,
+ * using a basic CRUD like pattern to manage an array of key to value pairs.
+ * 
+ * For this interface 
+ * * create is defined as register.
+ * * read is defined as registry (all containers), registered (really more of a
+ * check) and/or resolve (perhaps invoke is a better. 
+ * term, iCRUD?).
+ * * update is also defined as register as passing an exisiting container 
+ * name will overwrite it.
+ * * delete is defined as unregister
+ */
+class IoCRegistry implements IoCRegistryInterface
+{
+   /**
+    * An array of registered containers
+    * @var array
+    */
+    protected static $registry = array();
+
+    /**
+     * Adds a container to the registry.
+	 * 
+     * @param  string $name
+     * @param  object $container
+     * @return void
+     */
+    public static function register($name, $container)
+    {
+        self::$registry[$name] = $container;
+    }
+	
+	/**
+	 * Removes a container from the registry.
+	 * 
+	 * @param type $name
+	 * @retern void
+	 */
+	public static function unregister($name)
+	{
+		unset(self::$registry[$name]);
+	}
+	
+    /**
+     * Returns true if a given container has been registered.
+	 * 
+     * @param  string $name
+     * @return bool
+     */
+    public static function registered($name)
+    {
+        return array_key_exists($name, self::$registry);
+    }
+	
+	/**
+	 * Returns a list of all containers in the registry.
+	 * 
+	 * @return void
+	 */
+	public static function registry()
+	{
+		return self::$registry;
+	}
+ 
+	/**
+     * Instantiates a registered container. 
+	 * 
+	 * Instantiates a registered container. Throws an Exception if the 
+	 * container is not registered.
+	 * 
+     * @param  string $name
+     * @return mixed
+	 * @throws \Exception
+	 */
+    public static function resolve($name)
+    {
+		$obj = null;
+		
+        if (self::registered($name))
+        {
+            $obj = self::$registry[$name];
+            return $obj();
+        }
+ 
+        throw new \Exception('Container not found.');
+    }
+
+}
---
+++ core/Tinker/tests/Di/IoCRegistryTest.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Unit tests and mock objects for the IoCRegistry class
+ */
+
+/**
+ * Just a mock object to test container instantiation
+ */
+class IoCRegistryMockTestObject
+{
+    public function foo()
+    {
+        return 'bar';
+    }
+}
+
+/**
+ * Just a mock object to test container instantiation
+ */
+class IoCRegistryMockTestObjectToBeAdded
+{
+    public function foo()
+    {
+        return 'bar';
+    }
+}
+
+/**
+ * A unit test for the IoCRegistry class
+ */
+class IoCRegistryTest extends \PHPUnit_Framework_TestCase
+{
+
+	/**
+	 * We will start by adding a single container to the registery
+	 */
+    public function __construct()
+    {
+        //Add an initial container to the registry
+        \Tinker\Di\IoCRegistry::register('IoCRegistryMockTestObject', function() {
+            $IoCRegistryMockTestObject = new IoCRegistryMockTestObject; 
+            return $IoCRegistryMockTestObject;
+        });
+    }
+
+	/**
+	 * Test registry retrival by confirming the expected containers exist and
+	 * non-registered containers do not.
+	 */
+	public function testGetRegistry(){
+		$this->assertArrayHasKey('IoCRegistryMockTestObject', \Tinker\Di\IoCRegistry::registry());
+		$this->assertArrayNotHasKey('IoCRegistryMockTestObjectXXXXXXXXXX', \Tinker\Di\IoCRegistry::registry());
+	}
+	
+	/**
+	 * Test the ability to instantiate a container when the requested continer
+	 * exists and verify that calling an unregistered container will result in an error.
+	 * 
+	 * @expectedException Exception
+	 */
+	public function testCreateInstanceAnOfAIoCRegistry(){
+		
+        $IoCRegistryMockTestObject = \Tinker\Di\IoCRegistry::resolve('IoCRegistryMockTestObject');
+		$this->assertSame('bar', $IoCRegistryMockTestObject->foo());
+		
+		//Unregistered containers MUST throw an exception
+		$AssertWillThrowException = \Tinker\Di\IoCRegistry::resolve('ThisWillThrowAnException');
+		
+	}
+	
+	/**
+	 * Test adding new containers to the registry
+	 */
+	public function testAddAContainerToTheRegistry(){
+		
+		$this->assertFalse(\Tinker\Di\IoCRegistry::registered('IoCRegistryMockTestObjectToBeAdded'));
+		
+        \Tinker\Di\IoCRegistry::register('IoCRegistryMockTestObjectToBeAdded', function() {
+            $IoCRegistryMockTestObjectToBeAdded = new IoCRegistryMockTestObjectToBeAdded; 
+            return $IoCRegistryMockTestObjectToBeAdded;
+        });
+		
+		$this->assertArrayHasKey('IoCRegistryMockTestObjectToBeAdded', \Tinker\Di\IoCRegistry::registry());
+	}
+	
+	/**
+	 * Test removing containers from the registry
+	 */
+	public function testRemoveAContainerFromTheRegistry(){
+		
+		$count = count(\Tinker\Di\IoCRegistry::registry());
+		
+		$this->assertArrayHasKey('IoCRegistryMockTestObject', \Tinker\Di\IoCRegistry::registry());		
+
+        \Tinker\Di\IoCRegistry::unregister('IoCRegistryMockTestObject');
+		$this->assertArrayNotHasKey('IoCRegistryMockTestObject', \Tinker\Di\IoCRegistry::registry());
+		
+		//Make sure only one container is removed
+		$this->assertSame(count(\Tinker\Di\IoCRegistry::registry()), ($count - 1));
+	}
+}
+

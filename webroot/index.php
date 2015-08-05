fd6ca3eb808efed97d430fd93760b4f0f12c8014
Jason Snider <root@jasonsnider.com>
2015-08-05 09:13:05 -0500

    Fixed coding standards

--- webroot/index.php
+++ webroot/index.php
@@ -1,10 +1,12 @@
 <?php
+
 /**
  * index.php
  * 
  * The applications entry point. All requests will route through this file.
  * Set Apache's DocumentRoot to point to the webroot directory.
  */
+
 namespace Tinker;
 
 /**
@@ -19,7 +21,6 @@ define('ROOT', dirname(dirname(__FILE__)));
 
 //// Build time
 // I like to know how long things take. Start tracking the serer build time as soon as possible.
-
 //// Bootstrapping
 require ROOT . DS . 'config' . DS . 'bootstrap.php';
 
@@ -47,8 +48,16 @@ echo '</pre>';
 // 3. App, This will check the Plugin path inside App. This directory will ship empty by default. I'm assuming this 
 // is where you will build your application.
 // An autoloader will figure out which path to pull from.
-
 //Finally we will istanitate the classes based on the PSR-4 autoloading statndards. This might look something like:
 // $class = '\\Example\\Controller\\MainController';
 // $Controller = new $class();
 // $Controller->index();
+//Sample URI /tinker_plugin/tinker_plugin/execute/e1/e2/e3/e4:1
+$plugin = $Router->getPlugin(true);
+$controller = $Router->getPlugin(true) . 'Controller';
+$action = $Router->getAction();
+
+
+$class = "\\{$plugin}\\Controller\\{$controller}";
+$Controller = new $class();
+$Controller->{$action}();

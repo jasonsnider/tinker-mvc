01fe11f7f41c21a09f5aa581a2360d2023235c64
Jason Snider <root@jasonsnider.com>
2015-08-05 09:09:54 -0500

    Fixed coding standards

--- config/containers.php
+++ config/containers.php
@@ -2,12 +2,13 @@
 /**
  * A configuration file for DI containers.
  */
+
 namespace Tinker;
 
 //Add Router
-Di\IoCRegistry::register('Router', function(){
-	$Router = new Mvc\Router($_SERVER['REQUEST_URI']);
-	return $Router;
+Di\IoCRegistry::register('Router', function() {
+    $Router = new Mvc\Router($_SERVER['REQUEST_URI']);
+    return $Router;
 });
 
 ///// Custom Containers //////

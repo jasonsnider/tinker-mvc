65d7569efe131890dd73a711d050f0126b45cb07
Jason Snider <root@jasonsnider.com>
2016-09-16 17:33:11 -0500

    Added a camelCase metod to the Inflection trait

--- core/Tinker/src/Utility/Inflector.php
+++ core/Tinker/src/Utility/Inflector.php
@@ -11,12 +11,25 @@ trait Inflector
 {
 
     /**
-     * Converts a string to studly caps
+     * Converts a string to StudlyCaps
      * @param string $string
      * @return string
      */
     public function studlyCaps($string)
     {
         return str_replace('_', '', mb_convert_case($string, MB_CASE_TITLE, "UTF-8"));
+    }
+
+    /**
+     * Converts a string to camelCase
+     * @param string $string
+     * @return string
+     */
+    public function camelCase($string)
+    {
+        return preg_replace_callback('/(?!^)_([a-z])/', function($str)
+        {
+            return strtoupper($str[1]);
+        }, $string);
     }
 }
